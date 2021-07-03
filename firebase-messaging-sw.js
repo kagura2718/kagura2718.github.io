4ce61917e26a2fb40be842dbdb4fe22e16e8cc69
e Kagura <kagura2718@gmail.com>
2021-07-03 09:48:07 +0900

    automated commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,8 +4,8 @@
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
 
-importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-messaging.js');
 
 const firebaseConfig = {
   projectId: "kagura2718githubpages",
